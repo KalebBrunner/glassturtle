e863585ec0ee88742946e83bbe55dc02de58b6dd
Kaleb Brunner <127056216+KalebBrunner@users.noreply.github.com>
2026-03-08 23:56:55 -0400

    Code from "The Graphics Guy"'s youtube series worked but only when set to his toml settings.
    https://github.com/amengede/wgpu/blob/main/02-blank-screen/src/main.rs

--- main.rs
+++ main.rs
@@ -1,95 +1,182 @@
-use glfw::{
-    Action, Context, Key, VidMode, WindowHint, fail_on_errors,
-    ffi::{self, GLFW_POSITION_X},
-};
+use glfw::{Action, ClientApiHint, Key, Window, WindowHint, fail_on_errors};
 
-pub fn get_screen_mode(glfw: &mut glfw::Glfw) -> VidMode {
-    glfw.with_primary_monitor(|_, monitor_opt| {
-        let monitor = monitor_opt.expect("no primary monitor");
-        monitor.get_video_mode().expect("no video mode")
-    })
+struct State<'a> {
+    instance: wgpu::Instance,
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: (i32, i32),
+    window: &'a mut Window,
 }
 
-fn main() {
-    // activate glfw
-    /*
-       GLFW (Graphics Library Framework) is a lightweight utility library for
-       use with OpenGL, OpenGL ES and Vulkan. It provides programmers with the
-       ability to create and manage windows as well as OpenGL and Vulkan contexts,
-       as well as handle joystick, keyboard and mouse input.[3]
-    */
+impl<'a> State<'a> {
+    async fn new(window: &'a mut Window) -> Self {
+        let size = window.get_framebuffer_size();
+
+        let instance_descriptor = wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        };
+        let instance = wgpu::Instance::new(instance_descriptor);
+        let surface = instance.create_surface(window.render_context()).unwrap();
+
+        let adapter_descriptor = wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+        let adapter = instance.request_adapter(&adapter_descriptor).await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+            label: Some("Device"),
+        };
+        let (device, queue) = adapter
+            .request_device(&device_descriptor, None)
+            .await
+            .unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .filter(|f| f.is_srgb())
+            .next()
+            .unwrap_or(surface_capabilities.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.0 as u32,
+            height: size.1 as u32,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            instance,
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        }
+    }
+
+    fn resize(&mut self, new_size: (i32, i32)) {
+        if new_size.0 > 0 && new_size.1 > 0 {
+            self.size = new_size;
+            self.config.width = new_size.0 as u32;
+            self.config.height = new_size.1 as u32;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn update_surface(&mut self) {
+        self.surface = self
+            .instance
+            .create_surface(self.window.render_context())
+            .unwrap();
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let drawable = self.surface.get_current_texture()?;
+        let image_view_descriptor = wgpu::TextureViewDescriptor::default();
+        let image_view = drawable.texture.create_view(&image_view_descriptor);
+
+        let command_encoder_descriptor = wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        };
+        let mut command_encoder = self
+            .device
+            .create_command_encoder(&command_encoder_descriptor);
+
+        let color_attachment = wgpu::RenderPassColorAttachment {
+            view: &image_view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color {
+                    r: 0.75,
+                    g: 0.5,
+                    b: 0.25,
+                    a: 1.0,
+                }),
+                store: wgpu::StoreOp::Store,
+            },
+        };
+
+        let render_pass_descriptor = wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(color_attachment)],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        };
+
+        command_encoder.begin_render_pass(&render_pass_descriptor);
+        self.queue.submit(std::iter::once(command_encoder.finish()));
+
+        drawable.present();
+
+        Ok(())
+    }
+}
+
+async fn run() {
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();
-    println!("GLFW initiated");
-
-    // window creation hints
-    glfw.window_hint(WindowHint::TransparentFramebuffer(true));
-    glfw.window_hint(WindowHint::Decorated(true));
-    glfw.window_hint(WindowHint::MousePassthrough(true));
-
-    // create window and event handler
+    glfw.window_hint(WindowHint::ClientApi(ClientApiHint::NoApi));
     let (mut window, events) = glfw
-        .create_window(100, 100, "GlassTurtle", glfw::WindowMode::Windowed)
+        .create_window(800, 600, "It's WGPU time.", glfw::WindowMode::Windowed)
         .unwrap();
 
-    // set settings
-    window.set_key_polling(true);
-    window.make_current();
+    let mut state = State::new(&mut window).await;
 
-    // set location
-    let display = get_screen_mode(&mut glfw);
-    println!("Screen rez: {}x{}", display.width, display.height);
+    state.window.set_framebuffer_size_polling(true);
+    state.window.set_key_polling(true);
+    state.window.set_mouse_button_polling(true);
+    state.window.set_pos_polling(true);
 
-    let (posx, posy) = (
-        display.width as i32 - window.get_size().0,
-        display.height as i32 - window.get_size().1,
-    );
-    window.set_pos(posx, posy);
-    println!("Screen loc: ({}, {})", posx, posy);
-
-    // activate OpenGL
-    /*
-        OpenGL (Open Graphics Library[4]) is a cross-language, cross-platform
-        application programming interface (API) for rendering 2D and 3D vector
-        graphics. The API is typically used to interact with a graphics
-        processing unit (GPU), to achieve hardware-accelerated rendering.
-    */
-    gl::load_with(|symbol| {
-        window
-            .get_proc_address(symbol)
-            .map_or(std::ptr::null(), |p| p as *const _)
-    });
-
-    // Window event loop
-    while !window.should_close() {
+    while !state.window.should_close() {
         glfw.poll_events();
-
         for (_, event) in glfw::flush_messages(&events) {
-            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
-                window.set_should_close(true);
+            match event {
+                //Hit escape
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    state.window.set_should_close(true)
+                }
+
+                //Window was moved
+                glfw::WindowEvent::Pos(..) => {
+                    state.update_surface();
+                    state.resize(state.size);
+                }
+
+                //Window was resized
+                glfw::WindowEvent::FramebufferSize(width, height) => {
+                    state.update_surface();
+                    state.resize((width, height));
+                }
+                _ => {}
             }
         }
 
-        let (width, height) = window.get_framebuffer_size();
-
-        unsafe {
-            gl::Viewport(0, 0, width, height);
-
-            // transparent background
-            gl::ClearColor(0.0, 0.0, 0.0, 0.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-
-            // opaque red square
-            let square_size = (width.min(height) / 2).max(1);
-            let x = (width - square_size) / 2;
-            let y = (height - square_size) / 2;
-
-            gl::Enable(gl::SCISSOR_TEST);
-            gl::Scissor(x, y, square_size, square_size);
-            gl::ClearColor(1.0, 0.0, 0.0, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-            gl::Disable(gl::SCISSOR_TEST);
+        match state.render() {
+            Ok(_) => {}
+            Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                state.update_surface();
+                state.resize(state.size);
+            }
+            Err(e) => eprintln!("{:?}", e),
         }
-
-        window.swap_buffers();
     }
+}
+
+fn main() {
+    pollster::block_on(run());
 }
