1f400ea1b851e28b393f3d8688a8e216e654ef19
Kaleb Brunner <127056216+KalebBrunner@users.noreply.github.com>
2026-03-14 00:21:42 -0400

    add comments

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,22 @@ fn main() {
 }
 
 async fn run() {
+    /*
+    https://en.wikipedia.org/wiki/GLFW
+    GLFW (Good Luck Future Witches) is a lightweight utility library for
+    use with OpenGL, OpenGL ES and Vulkan. It provides programmers with
+    the ability to create and manage
+    - windows
+    - OpenGL contexts
+    - Vulkan contexts
+    as well as handle
+    - joysticks
+    - keyboards
+    - mice
+     */
     let (mut glfw, mut window, events) = setup();
+
+    // get custom state object
     let mut state = State::new(&mut window).await;
 
     while !state.window.should_close() {
