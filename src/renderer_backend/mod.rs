e25285c12fda55dad09e1fab598d994f37774fa3
Kaleb Brunner <127056216+KalebBrunner@users.noreply.github.com>
2026-03-14 23:08:12 -0400

    init
    https://docs.vulkan.org/tutorial/latest/02_Development_environment.html#_linux_2
    
    installed vulkan sdk as well

--- src/renderer_backend/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod pipeline_builder;
-pub use pipeline_builder::PipelineBuilder;
-pub mod mesh_builder;
--- src/while_window/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod keys;
-pub mod update_state;
-pub use keys::key_match;
-pub use update_state::update_state;
