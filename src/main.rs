ab00551b31c31c10d27b1878c2cb415708561700
Kaleb Brunner <127056216+KalebBrunner@users.noreply.github.com>
2026-03-15 00:01:34 -0400

    setup proj and create glfw
    https://docs.vulkan.org/tutorial/latest/03_Drawing_a_triangle/00_Setup/00_Base_code.html#_integrating_glfw

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,34 @@
-use winit::{
-    application::ApplicationHandler,
-    dpi::LogicalSize,
-    event::WindowEvent,
-    event_loop::{ActiveEventLoop, EventLoop},
-    window::{Window, WindowId},
-};
+mod glfw;
+use crate::glfw::initWindow;
 
-struct App {
-    window: Option<Window>,
-}
+async fn run() {
+    /*
+    https://en.wikipedia.org/wiki/GLFW
+    GLFW (Good Luck Future Witches) is a lightweight utility library for
+    use with OpenGL, OpenGL ES and Vulkan. It provides programmers with
+    the ability to create and manage
+    - windows
+    - OpenGL contexts
+    - Vulkan contexts
+    as well as handle
+    - joysticks
+    - keyboards
+    - mice
+     */
+    let (mut glfw, mut window, events) = initWindow();
+    print!("{:?}", window.get_context_version());
 
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        if self.window.is_some() {
-            return;
-        }
+    // get custom state object
+    // let mut state = State::new(&mut window).await;
 
-        let window_attributes = Window::default_attributes()
-            .with_title("vk_playground")
-            .with_inner_size(LogicalSize::new(960.0, 540.0));
+    while !window.should_close() {
+        glfw.poll_events();
 
-        let window = event_loop
-            .create_window(window_attributes)
-            .expect("failed to create window");
-
-        self.window = Some(window);
-
-        // --- Vulkan instance setup will go here later ---
-        //
-        // use std::sync::Arc;
-        // use vulkano::{
-        //     instance::{Instance, InstanceCreateInfo},
-        //     VulkanLibrary,
-        // };
-        //
-        // let library = VulkanLibrary::new()
-        //     .expect("failed to load Vulkan loader");
-        //
-        // let instance = Instance::new(
-        //     library,
-        //     InstanceCreateInfo::application_from_cargo_toml(),
-        // )
-        // .expect("failed to create Vulkan instance");
-        //
-        // println!("Vulkan instance created: {:?}", instance);
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        _window_id: WindowId,
-        event: WindowEvent,
-    ) {
-        match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::Resized(new_size) => {
-                println!("resized to {}x{}", new_size.width, new_size.height);
-            }
-            WindowEvent::RedrawRequested => {
-                // Rendering will go here later.
-            }
-            _ => {}
-        }
-    }
-
-    fn about_to_wait(&mut self, _event_loop: &ActiveEventLoop) {
-        if let Some(window) = &self.window {
-            window.request_redraw();
-        }
+        // key_match(&mut state, &events);
+        // update_state(&mut state)
     }
 }
 
 fn main() {
-    // println!("Hello world")
-    let event_loop = EventLoop::new().expect("failed to create event loop");
-
-    let mut app = App { window: None };
-
-    event_loop.run_app(&mut app).expect("event loop error");
+    pollster::block_on(run());
 }
