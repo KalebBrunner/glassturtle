1230250a687a228616ea86d1f9ac9b416a89dc73
Kaleb Brunner <127056216+KalebBrunner@users.noreply.github.com>
2026-05-04 17:26:36 -0400

    prepend GLFW to comments

--- src/myglfw.rs
+++ src/myglfw.rs
@@ -32,13 +32,12 @@ pub fn init_glfw() -> (
     let (mut window, events) = glfw
         .create_window(640, 480, "Glass Turtle.", glfw::WindowMode::Windowed)
         .unwrap();
-    println!("client api: {:?}", window.get_client_api());
-    println!("decorations?: {}", window.is_decorated());
+    println!("GLFW decorations?: {}", window.is_decorated());
     println!(
-        "transparent framebuffer: {}",
+        "GLFW transparent framebuffer: {}",
         window.is_framebuffer_transparent()
     );
-    println!("mouse passthrough?: {}", window.is_mouse_passthrough());
+    println!("GLFW mouse passthrough?: {}", window.is_mouse_passthrough());
     window.set_framebuffer_size_polling(true);
     window.set_key_polling(true);
     window.set_mouse_button_polling(true);
